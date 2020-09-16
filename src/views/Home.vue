63f09ad849cbc537a25de7a8da7444ea8d5661e1
Jason Hibbs <jason@jasonhibbs.co.uk>
2020-09-16 13:45:37 +0100

    works

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,110 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<template lang="pug">
+
+  main
+
+    p Good luck, Sam
+
+    form(
+      v-if="country"
+      @submit.prevent="onSubmit"
+    )
+
+      label.question
+        p What is the capital of:
+        p.country {{ country.name }}
+
+      .answer
+        input(
+          placeholder="Enter your guess"
+          spellcheck="false"
+          autocapitalize="off"
+          autocomplete="off"
+          autocorrect="off"
+          v-model="answer"
+        )
+        button(
+          :disabled="!answer.length"
+        ) Check
+
+    template(v-if="correct === true")
+      p That’s right!
+
+    template(v-if="correct === false")
+      p Oops, the answer is {{ country.capital }}
+
+
 </template>
-
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    HelloWorld
+interface CountryData {
+  name: string
+  capital: string
+}
+
+@Component
+export default class Home extends Vue {
+  api = 'https://restcountries.eu/rest/v2/all?fields=name;capital'
+  answer = ''
+  country: CountryData | null = null
+  correct: boolean | null = null
+
+  // Lifecycle
+
+  mounted() {
+    this.startNewQuestion()
   }
-})
-export default class Home extends Vue {}
+
+  // Questions
+
+  startNewQuestion() {
+    this.answer = ''
+    this.correct = null
+    this.getRandomCountry()
+  }
+
+  checkAnswer() {
+    this.correct =
+      this.forgivenString(this.answer) ===
+      this.forgivenString(this.country.capital)
+
+    setTimeout(() => {
+      this.startNewQuestion()
+    }, 2000)
+  }
+
+  // Utility
+
+  forgivenString(str: string) {
+    return str
+      .toLowerCase()
+      .replace('.', '')
+      .replace(',', '')
+      .replace('ç', 'c')
+      .replace('ē', 'e')
+      .replace('é', 'e')
+      .replace('ó', 'o')
+      .trim()
+  }
+
+  getRandomCountry() {
+    fetch(this.api)
+      .then(response => response.json())
+      .then(data => {
+        const length = data.length
+        const randomIndex = ~~(Math.random() * length)
+        const found = data[randomIndex]
+        this.country = found.capital ? found : data[randomIndex + 1]
+      })
+      .catch(error => {
+        console.warn(error)
+      })
+  }
+
+  // Events
+
+  onSubmit() {
+    this.checkAnswer()
+  }
+}
 </script>
